a56124fb1fbdc4934b2f6b33854fff1f6f788cf0
Adam Michoń <adammplan@gmail.com>
2023-05-19 11:45:10 +0200

    Boilerplate code

--- Renderer.cpp
+++ Renderer.cpp
@@ -1,4 +1,5 @@
 #include "Renderer.h"
+using namespace GameEngine;
 
 void Renderer::createWindow(unsigned int width, unsigned int height)
 {
@@ -7,10 +8,7 @@ void Renderer::createWindow(unsigned int width, unsigned int height)
 
 	SCREEN_WIDTH = width;
 	SCREEN_HEIGHT = height;
-}
 
-void Renderer::initialize()
-{
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
@@ -38,7 +36,10 @@ void Renderer::initialize()
 
     glEnable(GL_DEPTH_TEST);
 
+}
 
+void Renderer::initialize()
+{
     // build and compile our shader program
     // vertex shader
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -79,6 +80,33 @@ void Renderer::initialize()
     glDeleteShader(fragmentShader);
 }
 
+Renderer::Renderer() {
+    vertexShaderSource = R"glsl(
+	#version 430 core
+	
+	layout (location = 0) in vec3 Pos;
+	layout (location = 2) uniform mat4 u_ProjMatrix;
+	layout (location = 3) uniform mat4 u_ViewMatrix;
+	layout (location = 4) uniform mat4 u_ModelMatrix;
+	
+	void main()
+	{
+	   gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * vec4(Pos, 1.0);
+	}
+	)glsl";
+    fragmentShaderSource = R"glsl(
+	#version 430 core
+	
+	layout (location = 1) uniform vec4 TriangleColor;
+	
+	out vec4 FragColor;
+	void main()
+	{
+	   FragColor = TriangleColor;
+	}
+	)glsl";
+}
+
 Renderer::Renderer(
 const char* vertexShader = R"glsl(
 	#version 430 core
@@ -105,11 +133,13 @@ const char* fragmentShader = R"glsl(
 	}
 	)glsl") 
 {
-	if (instance > 0) throw std::logic_error("Only one instance allowed");
-	instance++;
+	//if (instance > 0) throw std::logic_error("Only one instance allowed");
+	//instance++;
 	vertexShaderSource = vertexShader;
 	fragmentShaderSource = fragmentShader;
 }
+
+
 
 void Renderer::framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
--- Renderer.h
+++ Renderer.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include<vector>
+#include "GameObject.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -10,23 +12,26 @@
 #include <iostream>
 
 #include <random>
-class Renderer
-{
-private:
-	static int instance;
-	const char* vertexShaderSource;
-	const char* fragmentShaderSource;
-	unsigned int SCREEN_WIDTH;
-	unsigned int SCREEN_HEIGHT;
-	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-public:
-	inline Renderer(const char*,const char*);
-	void createWindow(unsigned int, unsigned int);
-	void initialize();
+namespace GameEngine {
+	class Renderer
+	{
+	private:
+		std::vector<GameObject> listOfObjects;
+		//static int instance;
+		const char* vertexShaderSource;
+		const char* fragmentShaderSource;
+		unsigned int SCREEN_WIDTH;
+		unsigned int SCREEN_HEIGHT;
+		unsigned int VertexBufferId, VertexArrayId, ElementBufferId;
+		static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	public:
+		Renderer(const char*, const char*);
+		Renderer();
+		void createWindow(unsigned int, unsigned int);
+		void initialize();
+		void initializeObject();
 
-};
+	};
 
-int Renderer::instance = 0;
-
-
-
+	//int Renderer::instance = 0;
+}
